5e5d5996b576bf6a617b787d60d739e477c38c2e
VMChernov <vmchernov@inno.tech>
2025-09-12 14:56:26 +0300

    init

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        markdown = { "prettier" },
+        toml = { "taplo" },
+      },
+    },
+  },
+}
+
