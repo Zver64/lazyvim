9d1af4312dd9e0d6257b3cce96c484b0990f4bab
VMChernov <vmchernov@inno.tech>
2025-09-15 14:36:25 +0300

    keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set("n", "<leader>ce", function()
-  vim.cmd("EslintFixAll")
-end, { desc = "Fix all ESLint errors" })
+vim.keymap.set("i", "<C-Bslash>", function()
+  require("blink.cmp").show()
+end, { desc = "Trigger completion" })
