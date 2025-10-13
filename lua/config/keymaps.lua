2de059f5c2ace09f5771a0921bb2eb56b5dcac40
VMChernov <vmchernov@inno.tech>
2025-10-13 17:23:52 +0300

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,40 @@
 vim.keymap.set("i", "<C-Bslash>", function()
   require("blink.cmp").show()
 end, { desc = "Trigger completion" })
+
+-- Copy relative file path with line number
+vim.keymap.set("n", "<leader>apl", function()
+  local path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+  local line = vim.fn.line(".")
+  local result = string.format("%s:%d", path, line)
+  vim.fn.setreg("+", result)
+  vim.notify("Copied: " .. result)
+end, { desc = "Copy relative file path with line number" })
+
+-- Copy relative file path without line number
+vim.keymap.set("n", "<leader>apf", function()
+  local path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path)
+end, { desc = "Copy relative file path" })
+
+vim.keymap.set("n", "<leader>afd", function()
+  local current_file = vim.fn.expand("%:p")
+  local command = string.format("silent !rm %s", current_file)
+  vim.cmd(command)
+  vim.cmd("bdelete")
+  vim.notify("file deleted: " .. current_file)
+end, { desc = "Delete current file" })
+
+-- CTRL-q (insert-mode) manually triggers Amazon Q completion (inline suggestions).
+vim.keymap.set("i", "<c-q>", function()
+  local client = vim.lsp.get_clients({ bufnr = 0, name = "amazonq-completion" })[1]
+  if not client then
+    vim.notify("Amazon Q not enabled for this buffer")
+    return
+  end
+  vim.lsp.completion.enable(true, client.id, 0)
+  vim.notify("Amazon Q: working...")
+  vim.lsp.completion.get()
+  -- vim.cmd[[redraw | echo '']]
+end)
