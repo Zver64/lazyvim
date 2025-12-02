495f8feaa0cb5c66c4a8db3ea74f55da9303dbbe
VMChernov <vmchernov@inno.tech>
2025-12-02 14:06:10 +0300

    fixes and styles

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,13 +1,6 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    -- Добавляем форматирование для отображения имени источника в меню
-    formatting = {
-      format = function(entry, vim_item)
-        vim_item.menu = entry.source.name -- Здесь появится [LSP] или [Buffer]
-        return vim_item
-      end,
-    },
     keymap = {
       ["<C-Bslash>"] = { "show" },
     },
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,12 @@
+require("catppuccin").setup({
+    custom_highlights = function(colors)
+        return {
+            LineNr = { fg = colors.text }, -- Example: Set line numbers to Catppuccin's 'text' color
+            CursorLineNr = { fg = colors.peach }, -- Example: Set current line number to Catppuccin's 'peach' color
+        }
+    end,
+})
+
 return {
   { "ellisonleao/gruvbox.nvim" },
   {
