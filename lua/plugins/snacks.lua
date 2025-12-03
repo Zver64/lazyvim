acaa5a1b68fcc1d7ea252326d20077f6688e445c
VMChernov <vmchernov@inno.tech>
2025-12-03 11:18:13 +0300

    ability to remove a project via <C-x>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,27 +4,26 @@ return {
   ---@type snacks.Config
   opts = {
     picker = {
+      actions = {
+        delete_projects = function(picker, _)
+          Snacks.picker.actions.close(picker)
+          local items = picker:selected({ fallback = true })
+          vim.defer_fn(function()
+            vim.cmd("edit " .. vim.fn.stdpath("state") .. "/shada/main.shada")
+            for _, item in ipairs(items) do
+              local regex = "^\\S\\(\\n\\s\\|[^\\n]\\)\\{-}"
+                .. vim.fn.escape(item.file, "/\\")
+                .. "\\_.\\{-}\\n*\\ze\\(^\\S\\|\\%$\\)"
+              vim.cmd("%s/" .. regex .. "//g")
+            end
+            vim.cmd("write!")
+            vim.cmd("rshada!")
+            vim.cmd("bwipeout!")
+            Snacks.picker.projects()
+          end, 100)
+        end,
+      },
       sources = {
-        -- lsp_symbols = {
-        --   filter = {
-        --     typescript = {
-        --       "Class",
-        --       "Constructor",
-        --       "Enum",
-        --       "Field",
-        --       "Function",
-        --       "Interface",
-        --       "Method",
-        --       "Module",
-        --       "Namespace",
-        --       "Package",
-        --       "Property",
-        --       "Struct",
-        --       "Trait",
-        --       "Variable",
-        --     },
-        --   },
-        -- },
         files = {
           hidden = true,
         },
@@ -36,6 +35,15 @@ return {
         },
         grep_word = {
           hidden = true,
+        },
+        projects = {
+          win = {
+            input = {
+              keys = {
+                ["<C-x>"] = { "delete_projects", mode = { "n", "i" } },
+              },
+            },
+          },
         },
       },
     },
